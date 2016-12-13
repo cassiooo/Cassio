a6bdb6f9b2eda337590f93b3bb75c53a1c72a81d
cassiooo <cassiooo.vasco@gmail.com>
2016-12-13 08:58:31 -0200

    Livro

--- models/Livro.js
+++ models/Livro.js
@@ -4,7 +4,9 @@ var LivroSchema = new mongoose.Schema({
   titulo: String,
   autor: String,
   ano: Number,
-  isbn: Number
+  paginas: Number,
+  isbn: Number,
+  categoria: String
 });
 
 module.exports = mongoose.model('Livro', LivroSchema);
